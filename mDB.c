fc2a15f672fb3f05b857e0cf2d7cf34e0300e6d8
Dannymoses <dannymoses555@gmail.com>
2015-06-03 00:57:40 -0400

    added more functionality, if you can call it that

--- mDB.c
+++ mDB.c
@@ -1,7 +1,6 @@
 /*
 ** Simple database written for fun
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,16 +10,72 @@ typedef struct field {
 	// TODO: make it dynamically aloocate the string for the field
 } field;
 
-void read_record(field* fields)
+void read_record(field* fields, int num_fields)
 {
+	FILE* pFile;
+	char fileName[80];
+	printf("What is the name of the record? ");
+	scanf("%s", fileName);
+	pFile = fopen(fileName, "r");
+
+	if (pFile == NULL) {
+		perror("The following error has occurred");
+		exit(EXIT_FAILURE);
+	} else {
+
+
 }
 
-void write_record(field* fields)
+void write_record(field* fields, int num_fields)
 {
+	FILE* pFile;
+	char fileName[80];
+	printf("What is the name of the record? ");
+	scanf("%s", fileName);
+	pFile = fopen(fileName, "a+");
+	
+	if (pFile == NULL) {
+		perror("The following error has occurred");
+		exit(EXIT_FAILURE);
+	} else {	
+		for (int i = 0; i < num_fields; i++) 
+		{
+			char buffer[80];
+			printf("%s: ", fields[i].name);
+			scanf("%s", buffer);
+			fprintf(pFile, "%s\n", buffer);
+			strcpy(buffer, "");
+		}
+	}
+
+	printf("done writing to record %s\n", fileName);
+	fclose(pFile);
+}
+
+void delete_record()
+{
+	char recordName[80];
+	printf("What is the name of the record(**WARNING:FILE WILL BE DELETED**)? ");
+	scanf("%s", recordName);
+	remove(recordName);
+	printf("record successfully deleted\n");
 }
 
 void clear_record()
 {
+	FILE* pFile;
+	char recordName[80];
+	printf("What is the name of the record(the file itself will not be deleted)? ");
+	scanf("%s", recordName);
+	pFile = fopen(recordName, "w");
+	if (pFile == NULL) {
+		perror("The following error has occurred");
+		exit(EXIT_FAILURE);
+	} else {
+		freopen(recordName, "w", pFile);
+		printf("%s cleared successfully\n", recordName);
+	}
+	fclose(pFile);
 }
 
 void create_record()
@@ -34,27 +89,23 @@ void create_record()
 		perror("The following error has occurred");
 		exit(EXIT_FAILURE);
 	} else {
-		fprintf(pFile, "%s", "MOSESDB_RECORD");
+		fprintf(pFile, "%s", "MOSESDB_RECORD\n");
 	}
 	printf("record created successfully\n");
+	fclose(pFile);
 }
 
-void print_schema(FILE* pFile)
+void print_schema(field* fields, int num_fields)
 {
-	char buffer[80];
-	if (pFile != NULL) {
-		while (fscanf(pFile, "%s", buffer)) {
-		printf("%s", buffer);
-			if (feof(pFile))  break; 
-		}
-} else {
-	perror("The following error has accurred");
-			exit(EXIT_FAILURE);
+	for (int i = 0; i < num_fields; i++)
+	{
+		printf("%s\n", fields[i].name);
 	}
 }
 
-void clear_schema(FILE* pFile)
+void clear_schema(FILE* pFile, char fileName[80])
 {
+	pFile = freopen(fileName, "w", pFile);
 }
 
 int main(int argc, char **argv)
@@ -65,9 +116,10 @@ int main(int argc, char **argv)
 	char writeRecord[] = "WRITE_R";
 	char clearRecord[] = "CLEAR_R";
 	char createRecord[] = "CREATE_R";
+	char deleteRecord[] = "DELETE_R";
 	char printSchema[] = "PRINT_S";
 	char clearSchema[] = "CLEAR_S";
-	char command[10];
+	char command[100];
 	field* pFields; // dynamic allocation of fields anyone?
 	int numFields; // the number of fields
 	char newline[1];
@@ -103,17 +155,26 @@ int main(int argc, char **argv)
 		strcpy(command, "");
 		scanf("%s", command);
 		if (strcmp(command, readRecord) == 0) {
-			read_record(pFields);
+			read_record(pFields, numFields);
 		} else if (strcmp(command, writeRecord) == 0) {
-			write_record(pFields);
+			write_record(pFields, numFields);
 		} else if (strcmp(command, clearRecord) == 0) {
 			clear_record();
 		} else if (strcmp(command, createRecord) == 0) {
 			create_record();
 		} else if (strcmp(command, printSchema) == 0) {
-			print_schema(schemaFile);
+			print_schema(pFields, numFields);
 		} else if (strcmp(command, clearSchema) == 0) {
-			clear_schema(schemaFile);
+			clear_schema(schemaFile, argv[1]);
+		} else if (strcmp(command, deleteRecord) == 0) {
+			delete_record();
+		}else if (strcmp(command, "HELP") == 0) {
+				printf("TODO: make a help function\n");
+		} else if (strcmp(command, "EXIT") == 0) {
+			printf("Exiting...\n");
+			exit(EXIT_SUCCESS);
+		} else {
+			printf("Command not recognized, please try again.\n");
 		}
 	}
 
