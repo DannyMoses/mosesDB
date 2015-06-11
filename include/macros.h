f467e751825eecf8dec957ef6ffdad3adf3afe35
Dannymoses <dannymoses555@gmail.com>
2015-06-10 22:03:53 -0400

    Added some more macros. Expect Travis to break for this one.

--- include/macros.h
+++ include/macros.h
@@ -3,3 +3,14 @@
 #include <stdarg.h>
 
 #define prompt(args...) printf("mosesdb> "); printf(args);
+
+#define LOG(args...) \
+	FILE* log_file = fopen("mosesdb.log", "a"); \
+	if (log_file != NULL) { \
+		fprintf(log_file, "mosesdb log> "); \
+		fprintf(log_file, args); \
+	} else printf("LOG ERROR\n"); 
+
+#define DEBUG(args...) \
+	fprintf(stderr, "mosesdb debug> "); \
+	fprintf(stderr, args);
--- src/api.c
+++ src/api.c
@@ -2,11 +2,11 @@
 
 int lookup_field(field* fields, int num_fields, char* field_to_lookup) 
 {
-	// LOG("Beginning function call to lookup_field\n")
+	LOG("Beginning function call to lookup_field\n")
 	// yay null checking
 	if (fields == NULL || field_to_lookup == NULL) {
-		// DEBUG("Looks like we got a null pointer somewhere boys\n")
-		// LOG("Function call to lookup_field failed. Reason: ya dun fscked up with a null pointer somewhere\n")
+		DEBUG("Looks like we got a null pointer somewhere boys\n")
+		LOG("Function call to lookup_field failed. Reason: ya dun fscked up with a null pointer somewhere\n")
 	} else {
 		// carry on
 	}
@@ -22,30 +22,30 @@ int lookup_field(field* fields, int num_fields, char* field_to_lookup)
 	}
 
 	if (line_of_field == -1) {
-		// DEBUG("Did you specify the correct field?\n")
-		// LOG("Function call to lookup_field failed. Reason: bad field name\n")
+		DEBUG("Did you specify the correct field?\n")
+		LOG("Function call to lookup_field failed. Reason: bad field name\n")
 		return FUNCTION_ERROR;
 	} else {
 		// hell of a shot kid, that was one in a million
 	}
 	
-	// LOG("Function call to lookup_field returned success\n")
+	LOG("Function call to lookup_field returned success\n")
 	return line_of_field; // I don't thnk I need to explain what's happening here
 }
 
 int init_schema(field* fields, int num_fields, char* file_name)
 {
-	// LOG("Beginning function call to init_schema\n")
+	LOG("Beginning function call to init_schema\n")
 	FILE* schema_file;
 	
 	schema_file = fopen(file_name, "r");
 	
 	if (schema_file == NULL) {
-		// LOG("Function call to init_schema failed. Reason: bad file name\n")
-		// DEBUG("Did you specify the correct file name?\n")
+		LOG("Function call to init_schema failed. Reason: bad file name\n")
+		DEBUG("Did you specify the correct file name?\n")
 		return FUNCTION_FAILURE;
 	} else {
-		// LOG("new schema file opened successfully\n")
+		LOG("new schema file opened successfully\n")
 	}
 	
 	if (fields != NULL) {
@@ -60,11 +60,11 @@ int init_schema(field* fields, int num_fields, char* file_name)
 	fields = (field *) calloc(num_fields, sizeof(field));
 
 	if (fields == NULL) {
-		// LOG("The function call to init_schema failed. Reason: bad memory allocation.\n")
-		// DEBUG("big bad memory problem\n")
+		LOG("The function call to init_schema failed. Reason: bad memory allocation.\n")
+		DEBUG("big bad memory problem\n")
 		return FUNCTION_FAILURE;
 	} else {
-		// LOG("In function call to init_schema: the new fields array has been allocated successfully\n")
+		LOG("In function call to init_schema: the new fields array has been allocated successfully\n")
 	}
 
 	for (int i = 0; i <= num_fields; i++) {
@@ -72,7 +72,7 @@ int init_schema(field* fields, int num_fields, char* file_name)
 		m_getline(&fields[i].name, &n, schema_file);
 	}
 	
-	// LOG("Function call to init_schema returned success\n")
+	LOG("Function call to init_schema returned success\n")
 	
 	return num_fields;
 }
